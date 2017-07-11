861879d55f1316744a4f40effe2ad6297ee53bb2
Vladimir Novick <vlad.novick@gmail.com>
2017-07-11 17:10:09 +0300

    show log file from MEMORY MONITOR SERVICE

--- less.sh
+++ less.sh
@@ -1,2 +1,26 @@
 #!/bin/bash
+#
+# Author: Vladimir Novick , <vlad.novick@gmail.com>
+#
+#                          https://www.linkedin.com/in/vladimirnovick
+#                      
+# Date: 11.07.2017
+# Purpose: check max using memory and create log file
+#
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+echo ""
+echo "                 MEMORY MONITOR SERVICE"
+echo "                 ----------------------"
+echo ""
+echo  " Service provide periodical check max using memory and create log file"
+echo  "                File: /var/monitor/memory.log"
+echo  ""
 less /var/monitor/memory.log
